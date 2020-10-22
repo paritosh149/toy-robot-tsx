c7866f1dddf4c410b159dc298766cf5f1a52c3e7
Paritosh Mishra <paritosh149@outlook.com>
2020-10-23 00:36:06 +1100

    Board Size input added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React, { MouseEvent, useState } from 'react';
-import { RobotRunner } from './RobotRunner';
-import DropZone from './DropZone';
-import './styles.css';
+import React, { MouseEvent, useState } from "react";
+import { RobotRunner } from "./RobotRunner";
+import DropZone from "./DropZone";
+import "./styles.css";
 
 export default function App() {
-  const [inputData, setInputData] = useState<string>('');
-  const [outputData, setOutputData] = useState<string>('');
+  const [inputData, setInputData] = useState<string>("");
+  const [outputData, setOutputData] = useState<string>("");
+  const [sizeData, setSizeData] = useState({ x: 5, y: 5 });
   const run = (event: MouseEvent) => {
-    const runner = new RobotRunner(5, 5);
+    const runner = new RobotRunner(sizeData.x, sizeData.y);
     setOutputData(runner.run(inputData));
   };
   const reset = (event: MouseEvent) => {
-    setInputData('');
-    setOutputData('');
-    console.log('Reset... Done.');
+    setInputData("");
+    setOutputData("");
   };
 
   const addCommandLine = (command: string) => {
-    const newState = inputData + (inputData.length > 0 ? '\n' : '') + command;
+    const newState = inputData + (inputData.length > 0 ? "\n" : "") + command;
     setInputData(newState);
   };
 
@@ -33,6 +33,7 @@ export default function App() {
           <tr>
             <td>Input</td>
             <td>Output</td>
+            <td>Board Size</td>
           </tr>
         </thead>
         <tbody>
@@ -49,14 +50,44 @@ export default function App() {
             <td>
               <textarea rows={10} readOnly value={outputData} />
             </td>
+            <td>
+              <div>
+                X:
+                <input
+                  onChange={(e) => {
+                    setSizeData((state) => ({
+                      ...state,
+                      x: Number(e.target.value)
+                    }));
+                  }}
+                  value={sizeData.x}
+                  style={{ width: "20px" }}
+                />
+              </div>
+              <div>
+                Y:
+                <input
+                  onChange={(e) => {
+                    setSizeData((state) => ({
+                      ...state,
+                      y: Number(e.target.value)
+                    }));
+                  }}
+                  value={sizeData.y}
+                  style={{ width: "20px" }}
+                />
+              </div>
+            </td>
           </tr>
           <tr>
             <td colSpan={2}>
-              <button onClick={() => addCommandLine('PLACE')}>PLACE</button>
-              <button onClick={() => addCommandLine('MOVE')}>MOVE</button>
-              <button onClick={() => addCommandLine('LEFT')}>LEFT</button>
-              <button onClick={() => addCommandLine('RIGHT')}>RIGHT</button>
-              <button onClick={() => addCommandLine('REPORT')}>REPORT</button>
+              <button onClick={() => addCommandLine("PLACE 0,0,NORTH")}>
+                PLACE
+              </button>
+              <button onClick={() => addCommandLine("MOVE")}>MOVE</button>
+              <button onClick={() => addCommandLine("LEFT")}>LEFT</button>
+              <button onClick={() => addCommandLine("RIGHT")}>RIGHT</button>
+              <button onClick={() => addCommandLine("REPORT")}>REPORT</button>
               <button onClick={run}>Run</button>
               <button onClick={reset}>Reset</button>
             </td>
